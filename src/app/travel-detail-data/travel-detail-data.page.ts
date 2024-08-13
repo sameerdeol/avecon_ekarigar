5fcdaf5a5aa2eb5cc882515431b2e109a85d9fac
ashutosh <144018820+ashu6879@users.noreply.github.com>
2024-08-13 22:16:13 +0530

    13August

--- src/app/travel-detail-data/travel-detail-data.page.ts
+++ src/app/travel-detail-data/travel-detail-data.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 @Component({
   selector: 'app-travel-detail-data',
   templateUrl: './travel-detail-data.page.html',
@@ -11,5 +12,27 @@ export class TravelDetailDataPage implements OnInit {
 
   ngOnInit() {
   }
-
+  public generatePDF(): void {
+    const data = document.getElementById('downloadid');
+  
+    // Temporarily remove scrolling to capture the entire content
+    data!.style.overflow = 'visible';
+  
+    html2canvas(data!, { scrollX: 0, scrollY: -window.scrollY, width: data!.scrollWidth }).then(canvas => {
+      const imgWidth = 208;
+      const pageHeight = 295;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+  
+      const contentDataURL = canvas.toDataURL('image/png');
+      let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
+      let position = 0;
+      
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save('Table.pdf'); // Generated PDF
+      
+      // Restore the scrolling after capturing the content
+      data!.style.overflow = 'auto';
+    });
+  }
+  
 }
